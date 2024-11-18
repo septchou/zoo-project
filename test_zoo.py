7850187215388f40a4a709cd0c8e5fcace85f8f3
septchou <thichanonsittisomboon@gmail.com>
2024-11-18 17:52:36 +0000

    add karabown

--- test_zoo.py
+++ test_zoo.py
@@ -6,7 +6,11 @@ class TestZoo(unittest.TestCase):
         self.zoo = Zoo()
 
     def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
+        self.assertEqual(self.zoo.get_ticket_price(-1), "error")
+        self.assertEqual(self.zoo.get_ticket_price(1), 50)
+        self.assertEqual(self.zoo.get_ticket_price(15), 100)
+        self.assertEqual(self.zoo.get_ticket_price(54), 150)
+        self.assertEqual(self.zoo.get_ticket_price(67), 100)
        
     # Add your additional test cases here.
 
